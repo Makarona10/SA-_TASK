c5160f50a21ef2e9f65e8ccbee98a7023d28b381
Makarona10 <ahmedneyssi@gmail.com>
2024-04-21 02:26:38 +0200

    Task is done

--- client/server.js
+++ client/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static('public'));
 
